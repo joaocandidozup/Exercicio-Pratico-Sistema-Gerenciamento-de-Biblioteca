a3f46e6a649c35a3b3a7bd4244bbce7e6c966359
joaocandidozup <joao.candido@zup.com>
2024-12-18 09:30:49 -0300

    feat:add the Book entity

--- src/Livro.java
+++ src/Livro.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Livro {
+    //atributos
+    public String  titulo;
+    public String autor;
+    public String isbn;
+    public boolean disponivel;
+    //metodos
+    public void exibirDetalhes(){
+
+    }
+    public void emprestar(){
+
+    }
+    public void devolver(){
+
+    }
 }
