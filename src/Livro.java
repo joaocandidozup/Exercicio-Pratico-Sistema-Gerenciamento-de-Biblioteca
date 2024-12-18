5075f05cb3c2110badb70a904ded8f0839fe7fb7
joaocandidozup <joao.candido@zup.com>
2024-12-18 09:47:45 -0300

    add book class constructor and update attributes to private

--- src/Livro.java
+++ src/Livro.java
@@ -1,17 +1,28 @@
 public class Livro {
     //atributos
-    public String  titulo;
-    public String autor;
-    public String isbn;
-    public boolean disponivel;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean disponivel;
+
+    //construtor
+    public Livro(String titulo, String autor, String isbn, boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponivel = disponivel;
+    }
+
     //metodos
-    public void exibirDetalhes(){
+    public void exibirDetalhes() {
 
     }
-    public void emprestar(){
+
+    public void emprestar() {
 
     }
-    public void devolver(){
+
+    public void devolver() {
 
     }
 }
