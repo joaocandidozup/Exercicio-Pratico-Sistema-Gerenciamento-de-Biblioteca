6eccfa900df48f55bfddb1a753bd13b541f36dff
joaocandidozup <joao.candido@zup.com>
2024-12-19 10:03:02 -0300

    feat:feat: add entity Library

--- src/com/catalisa/biblioteca/Biblioteca.java
+++ src/com/catalisa/biblioteca/Biblioteca.java
@@ -1,4 +1,17 @@
 package com.catalisa.biblioteca;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+    //atributos
+    private ArrayList<Livro> livros;
+    private ArrayList<Usuario> usuarios;
+    //construtor
+    public void Biblioteca(){
+        livros = new ArrayList<Livro>();
+        usuarios = new ArrayList<Usuario>();
+    }
+    //metodos
+
+
 }
