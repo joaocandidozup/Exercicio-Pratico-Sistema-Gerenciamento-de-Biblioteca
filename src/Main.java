a7d526e0f507b56cec729652806ef0b6ae84b081
joaocandidozup <joao.candido@zup.com>
2024-12-19 09:46:24 -0300

    chore:add options menu logic

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
         int opcao = 0;
-        while(opcao !=6){
-            menu();
+        System.out.println(" Bem-vindo ao Sistema de Gerenciamento de Biblioteca!");
+        while (opcao != 6) {
+            exibeMenu();
             System.out.println("Digite uma opcao: ");
             opcao = leitura.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Cadastrar livro.");
+                    break;
+                case 2:
+                    System.out.println("Cadastrar usuário.");
+                    break;
+                case 3:
+                    System.out.println("Realizar empréstimo.");
+                    break;
+                case 4:
+                    System.out.println("Realizar devolução.");
+                    break;
+                case 5:
+                    System.out.println("Exibir livros disponíveis.");
+                    break;
+                default:
+                    System.out.println("Saindo da aplicação....");
+                    break;
+
+
+            }
         }
     }
-    public static void menu(){
+
+
+    public static void exibeMenu() {
         String menu = """
-                Bem-vindo ao Sistema de Gerenciamento de Biblioteca!
+                
                 1 - Cadastrar livro
                 2 - Cadastrar usuário
                 3 - Realizar emprestimo
